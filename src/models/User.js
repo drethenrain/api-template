0e65e055f7f890fbf65830e6a3d4778871947dbf
drethenrain <drethenrain@hotmail.com>
2021-08-08 19:56:58 -0300

    🕵️ Encrypted password

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,6 @@
 const { v4: uuid } = require('uuid');
 const { Schema, model } = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const schema = new Schema(
   {
@@ -25,5 +26,16 @@ const schema = new Schema(
     timestamps: true,
   }
 );
+
+schema.pre('save', async function hash(next) {
+  const user = this;
+  const hashed = await bcrypt.hash(user.password, 10);
+  user.password = hashed;
+  next();
+});
+
+schema.methods.comparePassword = function compare(password, cb) {
+  bcrypt.compare(password, this.password, (err, isMatch) => cb(err, isMatch));
+};
 
 module.exports = model('User', schema);
